531cc11a9fb3efd17da608c66cf89ce497d172b2
MisbahKhan0009 <misbah.khan@northsouth.edu>
2022-10-24 01:03:31 +0600

    Module 5 begin

---
+++ Module-5/Problem01_for.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int n, w, i = 0;
+    int sum = 0;
+    printf("Enter number of people: ");
+    scanf("%d", &n);
+
+    printf("Enter weight of %d people: ", n);
+
+   for ( i = 0; i < n; i++)
+   
+   
+    {
+        scanf("%d", &w);
+        sum += w;
+    }
+    printf("Sum of weight is: %d", sum);
+    return 0;
+}
---
+++ Module-5/Problem01_while.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int n, w, i = 0;
+    int sum = 0;
+    printf("Enter number of people: ");
+    scanf("%d", &n);
+
+    printf("Enter weight of %d people: ", n);
+
+    while (i<n)
+    {
+        scanf("%d", &w);
+        sum += w;
+        i++;
+    }
+    printf("Sum of weight is: %d", sum);
+    return 0;
+}
