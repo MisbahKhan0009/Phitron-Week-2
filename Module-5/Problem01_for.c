3933f482c1c19a054867f39de019096d953e81d9
MisbahKhan0009 <misbah.khan@northsouth.edu>
2022-10-28 12:32:10 +0600

    module 2 added

--- Module-5/Problem01_for.c
+++ Module-5/Problem01_for.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
 
-    int n, w, i = 0;
-    int sum = 0;
-    printf("Enter number of people: ");
-    scanf("%d", &n);
+  int n, w, i = 0;
+  int sum = 0;
+  printf("Enter number of people: ");
+  scanf("%d", &n);
 
-    printf("Enter weight of %d people: ", n);
+  printf("Enter weight of %d people: ", n);
 
-   for ( i = 0; i < n; i++)
-   
-   
-    {
-        scanf("%d", &w);
-        sum += w;
-    }
-    printf("Sum of weight is: %d", sum);
-    return 0;
+  for (i = 0; i < n; i++)
+
+  {
+    scanf("%d", &w);
+    sum += w;
+  }
+  printf("Sum of weight is: %d", sum);
+  return 0;
 }
--- Module-5/Problem01_while.c
+++ Module-5/Problem01_while.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
 
-    int n, w, i = 0;
-    int sum = 0;
-    printf("Enter number of people: ");
-    scanf("%d", &n);
+  int n, w, i = 0;
+  int sum = 0;
+  printf("Enter number of people: ");
+  scanf("%d", &n);
 
-    printf("Enter weight of %d people: ", n);
+  printf("Enter weight of %d people: ", n);
 
-    while (i<n)
-    {
-        scanf("%d", &w);
-        sum += w;
-        i++;
-    }
-    printf("Sum of weight is: %d", sum);
-    return 0;
+  while (i < n) {
+    scanf("%d", &w);
+    sum += w;
+    i++;
+  }
+  printf("Sum of weight is: %d", sum);
+  return 0;
 }
